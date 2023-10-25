ebcef9177850f57bed1a1061deb08e7f53a0f8f0
LucasLessa <lessa07oliveira@gmail.com>
2023-10-24 21:53:52 -0300

    Adicionando packages

--- apivsconnect/src/main/java/com/senai/apivsconnect/models/UsuarioModel.java
+++ apivsconnect/src/main/java/com/senai/apivsconnect/models/UsuarioModel.java
@@ -1,12 +1,12 @@
 package com.senai.apivsconnect.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -15,6 +15,18 @@ import java.io.Serializable;
 public class UsuarioModel implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_usuario", nullable = false)
+    private UUID id;
+
+    private String nome;
+    private String email;
+    private String senha;
+    private String endereco;
+    private String cep;
+    private String tipo_usuario;
 
 
 }
